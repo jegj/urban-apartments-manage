9db585a13de24914c016451d113995c918d841d2
Javier Galarza <jegj57@gmail.com>
2025-07-09 10:47:50 -0500

    feat: setup some frontend build steps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'frontend'],
+    ignores: ['eslint.config.mjs', 'frontend', 'vite.config.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'frontend',
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'frontend/main.js'),
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
+  },
+});

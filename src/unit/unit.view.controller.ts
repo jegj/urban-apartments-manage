b6254a6ac2d739e7f4761cfcf6f118be0bace3e0
Javier Galarza <jegj57@gmail.com>
2025-07-11 06:49:46 -0500

    feat: add more pages

--- src/unit/unit.view.controller.ts
+++ src/unit/unit.view.controller.ts
@@ -5,10 +5,28 @@ import { FastifyReply } from 'fastify';
 export class UnitViewController {
   constructor() {}
 
-  @Get('demo')
-  root(@Res() res: FastifyReply) {
+  @Get('demo/home')
+  GetHome(@Res() res: FastifyReply) {
     return res.view(
       'pages/home.unit.hbs',
+      { message: 'Hello world!' },
+      { layout: 'layouts/unit.hbs' },
+    );
+  }
+
+  @Get('demo/accommodation')
+  GetAccommodation(@Res() res: FastifyReply) {
+    return res.view(
+      'pages/accommodation.unit.hbs',
+      { message: 'Hello world!' },
+      { layout: 'layouts/unit.hbs' },
+    );
+  }
+
+  @Get('demo/tourist-places')
+  GetTouristPlaces(@Res() res: FastifyReply) {
+    return res.view(
+      'pages/tourist_places.unit.hbs',
       { message: 'Hello world!' },
       { layout: 'layouts/unit.hbs' },
     );

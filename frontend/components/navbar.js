eb0b7cf7b960c21bfd8a89d1acf247dfd0668331
Javier Galarza <jegj57@gmail.com>
2025-07-17 07:07:54 -0500

    refactor: remove translation

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,7 +1,20 @@
 export default () => ({
   active: false,
+  lang: 'en',
 
   init() {
+    const params = new URLSearchParams(window.location.search);
+    if (params.has('lang')) {
+      const lang = params.get('lang');
+      if (lang === 'en' || lang === 'es') {
+        this.lang = lang;
+      } else {
+        this.lang = 'en';
+      }
+    } else {
+      this.lang = 'en';
+    }
+
     window.addEventListener('scroll', () => {
       if (window.scrollY > 10) {
         this.active = true;

6ff745eb90698856d9f1aa0a1c973359a923988c
Javier Galarza <jegj57@gmail.com>
2025-07-22 07:23:22 -0500

    feat: show pw feat

---
+++ frontend/components/guest.login.js
@@ -0,0 +1,6 @@
+export default () => ({
+  errors: [],
+  showPassword: false,
+
+  init() {},
+});
--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,10 @@
 import Alpine from 'alpinejs';
 import navbar from './components/navbar';
+import guest from './components/guest.login';
 import './main.css';
 
 Alpine.data('navbar', navbar);
+Alpine.data('guest_login', guest);
 
 window.Alpine = Alpine;
 
